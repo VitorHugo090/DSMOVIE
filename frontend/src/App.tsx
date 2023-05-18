eaa7855e60b9c032eb707716e19899a8098cb177
Vitor_t <sinfo.estagiario03@tcmgo.tc.br>
2023-05-18 08:33:20 -0300

    Alteração do titulo do site e das cores

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import {ReactComponent as GithubIcon} from 'Vector.svg';
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
 import Navbar from 'components/Navbar';
+import './index.css';
 
 function App() {
 
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,8 +8,8 @@ function Navbar(){
         <header>
          <nav className="container">
          <div className="dsmovid-navbar-content" >
-         <h1>DsMovie</h1>
-       <a href="">
+         <h1>Letterbox - Plataforma Portifólio de Filmes</h1>
+       <a href="https://github.com/VitorHugo090">
          <div className="gitclass">
            <GithubIcon />
            <p className="gitclass-contact-link"> /VitorHugo</p>
