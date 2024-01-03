83a306676e35ec3fdd2669a7936e581da5e0649b
Vitor Hugo Fernandes <vitorhugofernandes035@gmail.com>
2024-01-03 15:35:02 -0300

    Vinculando API

--- frontend/src/components/CartaoFilme/index.tsx
+++ frontend/src/components/CartaoFilme/index.tsx
@@ -1,10 +1,22 @@
 
+import axios from "axios";
 import NotaFilme from "components/NotaFilme";
+import { error } from "console";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FilmService, axiosInstance } from "services/FilmService";
+const filmService = new FilmService();
 
 
 function CartaoFilme() {
-    
+    const [filme, setfilme] = useState({});
+    useEffect(()=> {
+      axios
+        .get('http://localhost:3000/filmes')
+        .then((response)=>setfilme(response.data))
+        .catch((error) => console.log(error))
+    }, [])
+
     const movie = {
         id: 1, 
         image: "https://4.bp.blogspot.com/-TH5qBbcjx5E/VhGc6OvO8PI/AAAAAAAAAZM/Yqe0mhOP3HM/s1600/whiplash.34229.jpg",
@@ -12,6 +24,7 @@ function CartaoFilme() {
         count: 2, 
         score: 5
     };
+    
 
     return (
 
@@ -25,6 +38,9 @@ function CartaoFilme() {
         </Link>
     </div>
     </div>
+
+
+
 
 );
 }
